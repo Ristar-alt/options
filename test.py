0a5a12a3f432b20605152f4224bfb47ee52bd698
Liao Song <songliao@foxmail.com>
2024-03-27 13:59:23 +0800

    add option type enums

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 import unittest
 
 import vanilla
+from vanilla import OptionType
 
 
 class TestVanilla(unittest.TestCase):
 
     def test_call(self):
-        call_pv = vanilla.pv("C", 100, 100, 0.05, 0.04, 0.25, 1.0)
+        call_pv = vanilla.pv(OptionType.CALL, 100, 100, 0.05, 0.04, 0.25, 1.0)
 
         self.assertAlmostEqual(call_pv, 11.7192658608191, 5)
 
     def test_put(self):
-        call_pv = vanilla.pv("P", 100, 100, 0.05, 0.04, 0.25, 1.0)
+        call_pv = vanilla.pv(OptionType.PUT, 100, 100, 0.05, 0.04, 0.25, 1.0)
 
         self.assertAlmostEqual(call_pv, 7.83722493597365, 5)
 
--- vanilla.py
+++ vanilla.py
@@ -1,12 +1,25 @@
+from enum import Enum
+
 import numpy as np
 from scipy.stats import norm
+
+
+class OptionType(Enum):
+    CALL = 1
+    PUT = 2
+
+
+class Greek(Enum):
+    DELTA = 1
+    GAMMA = 2
+    THETA = 3
 
 
 def pv(option_type, spot, strike, rf, b, vol, ttm):
     """
     calculate vanilla option pv
-    :param option_type: "C" or "P"
-    :type  option_type: str
+    :param option_type: `CALL` or `PUT`
+    :type  option_type: OptionType, default
     :param spot: spot price
     :type  spot: float
     :param strike: strike price
@@ -23,11 +36,8 @@ def pv(option_type, spot, strike, rf, b, vol, ttm):
     d2 = d1 - vol * np.sqrt(ttm)
 
     # Price of the call option
-    if option_type.lower() == "c":
+    if option_type == OptionType.CALL:
         return spot * np.exp(-(rf - b) * ttm) * norm.cdf(d1) - strike * np.exp(-rf * ttm) * norm.cdf(d2)
 
-    elif option_type.lower() == "p":
-        return strike * np.exp(-rf * ttm) * norm.cdf(-d2) - spot * np.exp(-(rf - b) * ttm) * norm.cdf(-d1)
-
     else:
-        raise ValueError("Unknown option type!")
+        return strike * np.exp(-rf * ttm) * norm.cdf(-d2) - spot * np.exp(-(rf - b) * ttm) * norm.cdf(-d1)
